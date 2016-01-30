ef28c55ac0de74f39dd9bc30db4ffccf03f05174
adamsimsy <adamsimsy@gmail.com>
2016-01-30 17:27:43 +0000

    connection details for service now working

--- src/Example/Example.Website/App_Start/Installers/ServicesInstaller.cs
+++ src/Example/Example.Website/App_Start/Installers/ServicesInstaller.cs
@@ -11,11 +11,13 @@ namespace Example.Website.App_Start.Installers
     {
         public void Install(IWindsorContainer container, IConfigurationStore store)
         {
-            var url = "http://192.168.99.100:32770/db/data";
-            //var url = "http://sitecore.sb09.stations.graphenedb.com:24789/db/data";
+            //var url = "http://192.168.99.100:32770/db/data";
+            //var user = "neo4j";
+            //var password = "Password1!";
 
-            var user = "neo4j";
-            var password = "Password1!";
+            var url = "http://sitecore.sb09.stations.graphenedb.com:24789/db/data";
+            var user = "sitecore";
+            var password = "w1c0t4yXWUV7dY8TotZ8";
 
             var client = new GraphClient(new Uri(url), user, password);
 
